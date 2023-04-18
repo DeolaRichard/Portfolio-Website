814c7c31409eaf4c41dc1d3fae2d3d81a03bd0b4
DeolaRichard <adekunlerichie5@gmail.com>
2023-04-18 12:26:41 +0100

    Added function that activates controlls

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const section = document.querySelectorAll(".section");
 const sectBtns = document.querySelectorAll(".controls");
 const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelectorAll(".main-content");
+
+
+function pageTransition(){
+    //Button click active class 
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+         let currentBtn = document.querySelectorAll('.active-btn');
+         currentBtn[0].className = currentBtn[0].className.replace('active-btn', ' ');
+         this.className += ' active-btn';
+        })
+    }
+}
+
+pageTransition();
+
